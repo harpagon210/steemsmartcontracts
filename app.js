cafb2401d0ac2c564b890b68ece1e238f2140a18
Harpagon210 <harpagon210@gmail.com>
2018-10-13 20:10:14 -0500

    fixing the replay function

--- app.js
+++ app.js
@@ -138,6 +138,8 @@ async function replayBlocksLog() {
   let res = await loadPlugin(database);
   if (res && res.payload === null) {
     res = await loadPlugin(blockchain);
+    await send(getPlugin(database),
+      { action: database.PLUGIN_ACTIONS.GENERATE_GENESIS_BLOCK, payload: conf });
     res = await send(getPlugin(blockchain),
       { action: blockchain.PLUGIN_ACTIONS.START_BLOCK_PRODUCTION });
     if (res && res.payload === null) {
