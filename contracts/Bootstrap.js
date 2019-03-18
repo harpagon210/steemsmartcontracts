317b2cb14e53dc874d3686270a35aba3ebf27317
harpagon210 <harpagon210@gmail.com>
2019-03-18 15:54:15 -0500

    adding basic dice contract

--- contracts/Bootstrap.js
+++ contracts/Bootstrap.js
@@ -1485,6 +1485,86 @@ class Bootstrap {
 
     transactions.push(new Transaction(genesisSteemBlock, 0, 'null', 'contract', 'deploy', JSON.stringify(contractPayload)));
 
+    // dice contract
+    contractCode = `
+    const STEEM_PEGGED_SYMBOL = 'STEEMP';
+    const CONTRACT_NAME = 'dice';
+
+    actions.createSSC = async (payload) => {
+        await api.db.createTable('params');
+    
+        const params = {};
+        params.houseEdge = "0.01";
+        params.minBet = "0.1";
+        params.maxBet = "100";
+        await api.db.insert('params', params);
+    }
+    
+    actions.roll = async (payload) => {
+        // get the action parameters
+        const { roll, amount } = payload;
+    
+        // check the action parameters
+        if (api.assert(roll && Number.isInteger(roll) && roll >= 2 && roll <= 96, 'roll must be an integer and must be between 2 and 96')
+            && api.assert(amount && typeof amount === 'string' && api.BigNumber(amount).dp() <= 3 && api.BigNumber(amount).gt(0), 'invalid amount')) {
+    
+            // get the contract parameters
+            const params = await api.db.findOne('params', {});
+    
+            // check that the amount bet is in thr allowed range
+            if (api.assert(api.BigNumber(amount).gte(params.minBet) && api.BigNumber(amount).lte(params.maxBet), 'amount must be between minBet and maxBet')) {
+                // request lock of amount STEEMP tokens
+                const res = await api.executeSmartContract('tokens', 'transferToContract', { symbol: STEEM_PEGGED_SYMBOL, quantity: amount, to: CONTRACT_NAME });
+    
+                // check if the tokens were locked
+                if (res.errors === undefined &&
+                    res.events && res.events.find(el => el.contract === 'tokens' && el.event === 'transferToContract' && el.data.from === api.sender && el.data.to === CONTRACT_NAME && el.data.quantity === amount && el.data.symbol === STEEM_PEGGED_SYMBOL) !== undefined) {
+                    
+                    // get a deterministic random number 
+                    const random = api.random();
+    
+                    // calculate the roll
+                    const randomRoll = Math.floor(random * 100) + 1;
+    
+                    // check if the dice rolled under "roll"
+                    if (randomRoll < roll ) {
+                        let multiplier = api.BigNumber(1)
+                            .minus(params.houseEdge)
+                            .multipliedBy(100)
+                            .dividedBy(roll);
+                        
+                        // calculate the number of tokens won
+                        const tokensWon = api.BigNumber(amount)
+                            .multipliedBy(multiplier)
+                            .toFixed(3, api.BigNumber.ROUND_DOWN);
+    
+                        // send the tokens out
+                        await api.transferTokens(api.sender, STEEM_PEGGED_SYMBOL, tokensWon, 'user');
+
+                        // emit an event
+                        api.emit('results', { 'memo': 'you won. roll: ' + randomRoll + ', your bet: ' + roll });
+                    } else {
+                      // emit an event
+                      api.emit('results', { 'memo': 'you lost. roll: ' + randomRoll + ', your bet: ' + roll });
+                    }
+                }
+                // else, 
+                // errors will be displayed in the logs of the transaction
+            }
+          }
+    }
+    `;
+
+    base64ContractCode = Base64.encode(contractCode);
+
+    contractPayload = {
+      name: 'dice',
+      params: '',
+      code: base64ContractCode,
+    };
+
+    transactions.push(new Transaction(genesisSteemBlock, 0, 'steemsc', 'contract', 'deploy', JSON.stringify(contractPayload)));
+
 
     // bootstrap transactions
     transactions.push(new Transaction(genesisSteemBlock, 0, STEEM_PEGGED_ACCOUNT, 'tokens', 'create', '{ "name": "STEEM Pegged", "symbol": "STEEMP", "precision": 3, "maxSupply": 1000000000000 }'));
---
+++ test/dice.js
@@ -0,0 +1,184 @@
+/* eslint-disable */
+const { fork } = require('child_process');
+const assert = require('assert');
+const fs = require('fs-extra');
+
+const database = require('../plugins/Database');
+const blockchain = require('../plugins/Blockchain');
+const { Block } = require('../libs/Block');
+const { Transaction } = require('../libs/Transaction');
+
+//process.env.NODE_ENV = 'test';
+
+const conf = {
+  chainId: "test-chain-id",
+  genesisSteemBlock: 2000000,
+  dataDirectory: "./test/data/",
+  databaseFileName: "database.db",
+  autosaveInterval: 0,
+  javascriptVMTimeout: 10000,
+};
+
+let plugins = {};
+let jobs = new Map();
+let currentJobId = 0;
+
+function cleanDataFolder() {
+  fs.emptyDirSync(conf.dataDirectory);
+}
+
+function send(pluginName, from, message) {
+  const plugin = plugins[pluginName];
+  const newMessage = {
+    ...message,
+    to: plugin.name,
+    from,
+    type: 'request',
+  };
+  currentJobId += 1;
+  newMessage.jobId = currentJobId;
+  plugin.cp.send(newMessage);
+  return new Promise((resolve) => {
+    jobs.set(currentJobId, {
+      message: newMessage,
+      resolve,
+    });
+  });
+}
+
+
+// function to route the IPC requests
+const route = (message) => {
+  const { to, type, jobId } = message;
+  if (to) {
+    if (to === 'MASTER') {
+      if (type && type === 'request') {
+        // do something
+      } else if (type && type === 'response' && jobId) {
+        const job = jobs.get(jobId);
+        if (job && job.resolve) {
+          const { resolve } = job;
+          jobs.delete(jobId);
+          resolve(message);
+        }
+      }
+    } else if (type && type === 'broadcast') {
+      plugins.forEach((plugin) => {
+        plugin.cp.send(message);
+      });
+    } else if (plugins[to]) {
+      plugins[to].cp.send(message);
+    } else {
+      console.error('ROUTING ERROR: ', message);
+    }
+  }
+};
+
+const loadPlugin = (newPlugin) => {
+  const plugin = {};
+  plugin.name = newPlugin.PLUGIN_NAME;
+  plugin.cp = fork(newPlugin.PLUGIN_PATH, [], { silent: true });
+  plugin.cp.on('message', msg => route(msg));
+  plugin.cp.stdout.on('data', data => console.log(`[${newPlugin.PLUGIN_NAME}]`, data.toString()));
+  plugin.cp.stderr.on('data', data => console.error(`[${newPlugin.PLUGIN_NAME}]`, data.toString()));
+
+  plugins[newPlugin.PLUGIN_NAME] = plugin;
+
+  return send(newPlugin.PLUGIN_NAME, 'MASTER', { action: 'init', payload: conf });
+};
+
+const unloadPlugin = (plugin) => {
+  plugins[plugin.PLUGIN_NAME].cp.kill('SIGINT');
+  plugins[plugin.PLUGIN_NAME] = null;
+  jobs = new Map();
+  currentJobId = 0;
+}
+
+const STEEM_PEGGED_ACCOUNT = 'steemsc';
+
+// dice
+describe('dice', () => {
+  it('makes you win', (done) => {
+    new Promise(async (resolve) => {
+      cleanDataFolder();
+
+      await loadPlugin(database);
+      await loadPlugin(blockchain);
+
+      await send(database.PLUGIN_NAME, 'MASTER', { action: database.PLUGIN_ACTIONS.GENERATE_GENESIS_BLOCK, payload: conf });
+
+      let transactions = [];
+      transactions.push(new Transaction(30983000, 'TXID1233', 'harpagon', 'steempegged', 'buy', `{ "recipient": "${STEEM_PEGGED_ACCOUNT}", "amountSTEEMSBD": "1100.00 STEEM", "isSignedWithActiveKey": true }`));
+      transactions.push(new Transaction(30983000, 'TXID1234', 'harpagon', 'tokens', 'transferToContract', '{ "symbol": "STEEMP", "to": "dice", "quantity": "1000", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(30983000, 'TXID1236', 'satoshi', 'steempegged', 'buy', `{ "recipient": "${STEEM_PEGGED_ACCOUNT}", "amountSTEEMSBD": "100.00 STEEM", "isSignedWithActiveKey": true }`));
+      transactions.push(new Transaction(30983000, 'TXID1237', 'satoshi', 'dice', 'roll', `{ "roll": 95, "amount": "33" , "isSignedWithActiveKey": true }`));
+
+      let block = new Block(30983000, 'ABCD2', 'ABCD1', '2018-06-01T00:00:00', transactions);
+
+      await send(blockchain.PLUGIN_NAME, 'MASTER', { action: blockchain.PLUGIN_ACTIONS.PRODUCE_NEW_BLOCK_SYNC, payload: block });
+
+      let res = await send(database.PLUGIN_NAME, 'MASTER', {
+        action: database.PLUGIN_ACTIONS.GET_TRANSACTION_INFO,
+        payload: 'TXID1237'
+      });
+
+      const tx = res.payload;
+
+      const logs = JSON.parse(tx.logs);
+
+      const event = logs.events.find(ev => ev.contract === 'dice' && ev.event == 'results').data;
+
+      assert.equal(event.memo, "you won. roll: 5, your bet: 95");
+
+      resolve();
+    })
+      .then(() => {
+        unloadPlugin(blockchain);
+        unloadPlugin(database);
+        done();
+      });
+  });
+
+  it('makes you lose', (done) => {
+    new Promise(async (resolve) => {
+      cleanDataFolder();
+
+      await loadPlugin(database);
+      await loadPlugin(blockchain);
+
+      await send(database.PLUGIN_NAME, 'MASTER', { action: database.PLUGIN_ACTIONS.GENERATE_GENESIS_BLOCK, payload: conf });
+
+      let transactions = [];
+   
+      transactions.push(new Transaction(30983000, 'TXID1233', 'harpagon', 'steempegged', 'buy', `{ "recipient": "${STEEM_PEGGED_ACCOUNT}", "amountSTEEMSBD": "1100.00 STEEM", "isSignedWithActiveKey": true }`));
+      transactions.push(new Transaction(30983000, 'TXID1234', 'harpagon', 'tokens', 'transferToContract', '{ "symbol": "STEEMP", "to": "dice", "quantity": "1000", "isSignedWithActiveKey": true }'));
+      transactions.push(new Transaction(30983000, 'TXID1236', 'satoshi', 'steempegged', 'buy', `{ "recipient": "${STEEM_PEGGED_ACCOUNT}", "amountSTEEMSBD": "100.00 STEEM", "isSignedWithActiveKey": true }`));
+      transactions.push(new Transaction(30983000, 'TXID1237', 'satoshi', 'dice', 'roll', `{ "roll": 2, "amount": "33" , "isSignedWithActiveKey": true }`));
+
+      let block = new Block(30983000, 'ABCD2', 'ABCD1', '2018-06-01T00:00:00', transactions);
+
+      await send(blockchain.PLUGIN_NAME, 'MASTER', { action: blockchain.PLUGIN_ACTIONS.PRODUCE_NEW_BLOCK_SYNC, payload: block });
+
+      let res = await send(database.PLUGIN_NAME, 'MASTER', {
+        action: database.PLUGIN_ACTIONS.GET_TRANSACTION_INFO,
+        payload: 'TXID1237'
+      });
+
+      const tx = res.payload;
+
+      const logs = JSON.parse(tx.logs);
+
+      const event = logs.events.find(ev => ev.contract === 'dice' && ev.event == 'results').data;
+
+      assert.equal(event.memo, "you lost. roll: 5, your bet: 2");
+
+      resolve();
+    })
+      .then(() => {
+        unloadPlugin(blockchain);
+        unloadPlugin(database);
+        done();
+      });
+  });
+});
+
