5b2a6fbbd99b46e107259b7b534963ab054c1b32
harpagon210 <harpagon210@gmail.com>
2019-03-27 16:21:36 -0500

    improving extraction block script

--- extractBlocksLog.sh
+++ extractBlocksLog.sh
@@ -2,7 +2,6 @@
 
 read -p "Enter the mongodb container name : " containerName
 echo "Extracting blocks from $containerName"
-docker exec $containerName /bin/sh -c 'cd /data/'
-docker exec $containerName /bin/sh -c 'mongoexport -d ssc -c chain -o blocks.log'
-docker cp $containerName:/data/blocks.log .
+docker exec $containerName /bin/sh -c 'mongoexport -d ssc -c chain -o /data/blocks.log'
+docker cp $containerName:/data/blocks.log ./
 echo "Done extracting blocks from $containerName"
