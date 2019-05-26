c789239cfd6ff00214c3068c178787ddab230060
Steemdevelopment <Steemdevelopment@gmail.com>
2019-05-26 12:59:59 -0700

    Readding adding sscstore

--- contracts/bootstrap/Bootstrap.js
+++ contracts/bootstrap/Bootstrap.js
@@ -12,6 +12,8 @@ class Bootstrap {
     let contractPayload;
 
     const FORK_BLOCK_NUMBER = 33225948;
+    const SSC_STORE_PRICE = '0.001';
+    const SSC_STORE_QTY = '0.001';
 
     // tokens contract
     contractCode = await fs.readFileSync('./contracts/bootstrap/tokens.js');
@@ -25,6 +27,26 @@ class Bootstrap {
 
     contractPayload = {
       name: 'tokens',
+      params: '',
+      code: base64ContractCode,
+    };
+
+    transactions.push(new Transaction(genesisSteemBlock, 0, 'rocketx', 'contract', 'deploy', JSON.stringify(contractPayload)));
+
+    // sscstore contract
+    contractCode = await fs.readFileSync('./contracts/bootstrap/sscstore.js');
+    contractCode = contractCode.toString();
+
+    contractCode = contractCode.replace(/'\$\{BP_CONSTANTS.UTILITY_TOKEN_PRECISION\}\$'/g, BP_CONSTANTS.UTILITY_TOKEN_PRECISION);
+    contractCode = contractCode.replace(/'\$\{BP_CONSTANTS.UTILITY_TOKEN_SYMBOL\}\$'/g, BP_CONSTANTS.UTILITY_TOKEN_SYMBOL);
+    contractCode = contractCode.replace(/'\$\{FORK_BLOCK_NUMBER\}\$'/g, FORK_BLOCK_NUMBER);
+    contractCode = contractCode.replace(/'\$\{SSC_STORE_PRICE\}\$'/g, SSC_STORE_PRICE);
+    contractCode = contractCode.replace(/'\$\{SSC_STORE_QTY\}\$'/g, SSC_STORE_QTY);
+
+    base64ContractCode = Base64.encode(contractCode);
+
+    contractPayload = {
+      name: 'sscstore',
       params: '',
       code: base64ContractCode,
     };
