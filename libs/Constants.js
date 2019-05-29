94fd5234825689bf640105cb7a88adc25f64c64d
harpagon210 <harpagon210@gmail.com>
2019-05-29 13:27:18 -0500

    updating constants

--- libs/Constants.js
+++ libs/Constants.js
@@ -1,17 +1,21 @@
 const CONSTANTS = {
   // utility token definition
-  UTILITY_TOKEN_SYMBOL: 'ENG',
+  UTILITY_TOKEN_SYMBOL: 'ENG', // mainnet
+  // UTILITY_TOKEN_SYMBOL: 'SSC', // testnet
   UTILITY_TOKEN_PRECISION: 8,
 
   // pegged token definition
   STEEM_PEGGED_SYMBOL: 'STEEMP',
-  STEEM_PEGGED_ACCOUNT: 'steem-peg',
+  STEEM_PEGGED_ACCOUNT: 'steem-peg', // mainnet
+  // STEEM_PEGGED_ACCOUNT: 'steemsc', // testnet
 
   // default values
   ACCOUNT_RECEIVING_FEES: 'steemsc',
-  INITIAL_TOKEN_CREATION_FEE: '100',
+  INITIAL_TOKEN_CREATION_FEE: '100', // mainnet
+  // INITIAL_TOKEN_CREATION_FEE: '0', // testnet
   SSC_STORE_PRICE: '0.001',
-  SSC_STORE_QTY: '0.001',
+  SSC_STORE_QTY: '0.001', // mainnet
+  // SSC_STORE_QTY: '1', // testnet
 
   // forks definitions
   FORK_BLOCK_NUMBER: 30896500,
