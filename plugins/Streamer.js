9ba808144eb3a79b9b4c64e0ade434c27db94c2e
harpagon210 <harpagon210@gmail.com>
2020-04-06 15:30:51 -0500

    fix streamer

--- plugins/Streamer.js
+++ plugins/Streamer.js
@@ -313,7 +313,9 @@ const streamBlocks = async (reject) => {
           addBlockToBuf = true;
         } else {
           buffer.clear();
-          throw new ForkException(`a fork happened between block ${currentSteemBlock - 1} and block ${currentSteemBlock}`);
+          const msg = `a fork happened between block ${currentSteemBlock - 1} and block ${currentSteemBlock}`;
+          currentSteemBlock = lastBlockSentToBlockchain + 1;
+          throw new ForkException(msg);
         }
       } else {
         // get the previous block
